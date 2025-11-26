d6c14b4f55060ad3caaab117574a90bf7a0da809
Arnab Pramanik <165245105+Arnab141@users.noreply.github.com>
2025-11-26 10:17:07 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -49,6 +49,7 @@ function AppRoutes() {
           />
         </div>
       )}
+      <footer class=" py-4 bg-gray-900 text-white text-sm">© 2025 Arnab Pramanik. All rights reserved.</footer>
     </main>
   );
 }
@@ -66,7 +67,7 @@ function App() {
       <div className="flex min-h-screen bg-gray-100 font-sans">
         <Sidebar />
         <AppRoutes />
-        <footer class=" py-4 bg-gray-900 text-white text-sm">© 2025 Arnab Pramanik. All rights reserved.</footer>
+        
       </div>
     </Router>
   );
